57996d7e6e87e6ca21f623f47d723222a01125f1
Nitin_ranga <nitran.2011@gmail.com>
2024-09-15 16:37:37 +0530

    Test

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 30000,
+  responseTimeout: 30000,
+  requestTimeout: 30000,
   e2e: {
+    experimentalStudio:true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -14,5 +18,6 @@ module.exports = defineConfig({
     API_URL: process.env.CYPRESS_API_URL,
     USERNAME: process.env.CYPRESS_USERNAME,
     PASSWORD: process.env.CYPRESS_PASSWORD,
+    MAILSLURP_API_KEY: process.env.CYPRESS_MAILSLURP_API_KEY,
   },
 });
